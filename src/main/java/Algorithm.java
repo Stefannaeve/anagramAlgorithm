0039c705cf8fec92da1ac04fadd7fd9a111ee1a5
Stefan <stefan.naeve@hotmail.com>
2023-09-03 15:31:46 +0200

    Finished finding anagrams, could be optimized. but will finish here.

--- src/main/java/Algorithm.java
+++ src/main/java/Algorithm.java
@@ -1,15 +1,13 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Algorithm {
 
     HashMap<String, String> hashMap = new HashMap<>();
-    public void sorting(String string){
-            char[] sortArray = string.toLowerCase().toCharArray();
-            Arrays.sort(sortArray);
-            ArrayList<Character> tempArray = new ArrayList<>();
+
+    public void sorting(String string) {
+        char[] sortArray = string.toLowerCase().toCharArray();
+        Arrays.sort(sortArray);
+        ArrayList<Character> tempArray = new ArrayList<>();
         for (char cha : sortArray) {
             if (cha != ' ') {
                 tempArray.add(cha);
@@ -19,18 +17,33 @@ public class Algorithm {
         for (int i = 0; i < tempArray.size(); i++) {
             sortedArray[i] = tempArray.get(i);
         }
-            String sortedString = new String(sortedArray);
-        System.out.println(sortedString);
-            hashMap.put(string, sortedString);
+        String sortedString = new String(sortedArray);
+        hashMap.put(string, sortedString);
     }
-    public void getList(List<String> unsortedList){
+
+    public void getList(List<String> unsortedList) {
 
         for (String s : unsortedList) {
             sorting(s);
         }
-        System.out.println(" ");
-        for (String key : hashMap.keySet()) {
-            System.out.println(key + " = " + hashMap.get(key));
+        findAnagrams();
+    }
+
+    public void findAnagrams() {
+        HashMap<String, List<String>> anagramHashMap = new HashMap<>();
+        for (String i : hashMap.keySet()) {
+            List<String> anagramList = new ArrayList<>();
+            String valueI = hashMap.get(i);
+            for (String j : hashMap.keySet()) {
+                String valueJ = hashMap.get(j);
+                if (!Objects.equals(i, j) && Objects.equals(valueI, valueJ)) {
+                    anagramList.add(j);
+                }
+            }
+            anagramHashMap.put(i, anagramList);
+        }
+        for (String key : anagramHashMap.keySet()) {
+            System.out.println(key + " = " + anagramHashMap.get(key));
         }
     }
 }
