18f57687f4bf0e90bed7dc2e441272eb0efe159e
Stefan <stefan.naeve@hotmail.com>
2023-09-03 14:34:41 +0200

    Fixed the sorting algorithm, and sout for hashmap

--- src/main/java/Algorithm.java
+++ src/main/java/Algorithm.java
@@ -1,12 +1,42 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 public class Algorithm {
+
+    HashMap<String, String> hashMap = new HashMap<String, String>();
     public void sorting(String string){
-        char[] sortArray = string.toLowerCase().toCharArray();
-        int size = sortArray.length;
-        Arrays.sort(sortArray);
-        String sortedString = Arrays.toString(sortArray);
+            String temp = string;
+            char[] sortArray = temp.toLowerCase().toCharArray();
+            Arrays.sort(sortArray);
+            ArrayList<Character> tempArray = new ArrayList<>();
+        for (int i = 0; i < sortArray.length; i++) {
+            if (sortArray[i] != ' '){
+            tempArray.add(sortArray[i]);
+            }
+        }
+        char[] sortedArray = new char[tempArray.size()];
+        for (int i = 0; i < tempArray.size(); i++) {
+            sortedArray[i] = tempArray.get(i);
+        }
+            String sortedString = new String(sortedArray);
         System.out.println(sortedString);
+            hashMap.put(sortedString, string);
+    }
+    public void getList(List<String> unsortedList){
+
+        for (int i = 0; i < unsortedList.size(); i++) {
+            sorting(unsortedList.get(i));
+        }
+        System.out.println(" ");
+        for (String key : hashMap.keySet()) {
+            System.out.print(hashMap.get(key) + " = ");
+            System.out.println(key);
+        }
+    }
+    public void sortHashmap(){
+
     }
     public boolean abc(){
         return true;
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,19 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Algorithm algorithm = new Algorithm();
-        algorithm.sorting("Benjamin");
+        List<String> list = new ArrayList<String>();
+
+        list.add("Erling Jevne");
+        list.add("Peter Tom");
+        list.add("Linjedommervas");
+        list.add("Trompete");
+        list.add("Jeg vil renne");
+        list.add("Mons Ivar Mjelde");
+
+        algorithm.getList(list);
     }
 }
